45a471ced5dce9bd15c19a8b08ef05b6fbefc485
Mohmmadnuh Mujawar <mohmmadnuh1@gmail.com>
2026-02-02 13:44:37 +0530

    DOM

---
+++ Classwork/DOM/Dom.js
@@ -0,0 +1,4 @@
+//Document Object Model, a programming interface for HTML and XML documents. It represents the structure of a webpage and allows JavaScript to dynamically access and update the content, structure, and style of a webpage.
+//DOM is  breach between HTML and Javascript
+//We learn 4 javascript DOM methods to access and manipulate HTML elements
+//Javascript use DOM to interact with HTML
